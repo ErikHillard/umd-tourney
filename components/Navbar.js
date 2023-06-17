96ecfcc8a0690515a2a3ce7daa966675caca7ea7
Erik Hillard Laptop <erikhillard2018@gmail.com>
2023-06-17 13:49:17 -0400

    More small changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,6 @@ export default async function Navbar(  ) {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="min-w-full flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">UMD Tournament Runner</span>
         </Link>
         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -23,9 +22,11 @@ export default async function Navbar(  ) {
               <Link href="/pools" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Pools</Link>
             </li>
             {pools.map((pool) => (
-                <li>
-                  <Link href={"/pools/" + pool.poolName} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{"Pool " + pool.poolName.charAt(pool.poolName.length - 1)}</Link>
-                </li>
+                <div key={pool.poolName}>
+                  <li>
+                    <Link href={"/pools/" + pool.poolName} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{"Pool " + pool.poolName.charAt(pool.poolName.length - 1)}</Link>
+                  </li>
+                </div>
             ))}
             <li>
               <Link href="/teams" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Teams</Link>
