b43aa9fce6e707a79708623a0aa0b676b1fefe39
Erik Hillard Laptop <erikhillard2018@gmail.com>
2023-07-31 02:35:44 -0400

    Turned off dynamic nav bar to see if it will fix itself

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,7 +3,7 @@ import { getAllPools } from "../utils/apiUtils";
 
 export default async function Navbar(  ) {
 
-  const pools = await getAllPools();
+  // const pools = await getAllPools();
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -26,13 +26,13 @@ export default async function Navbar(  ) {
             <li>
               <Link href="/teams" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Teams</Link>
             </li>
-            {pools.map((pool) => (
+            {/* {pools.map((pool) => (
             <div key={pool.name}>
               <li>
                 <Link href={"/pools/" + pool.id} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{pool.name}</Link>
               </li>
             </div>
-        ))}
+        ))} */}
             <li>
               <Link href="/teams" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Teams</Link>
             </li>
