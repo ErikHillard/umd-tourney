e9d4691d8ff631d7d2e15924b7a1deb1a1b61fad
Erik Hillard Laptop <erikhillard2018@gmail.com>
2023-09-26 23:59:25 -0400

    Setup useMemo for the currentSet as well as the loading spinner

--- app/(site)/matches/[id]/ClientMatchPage.jsx
+++ app/(site)/matches/[id]/ClientMatchPage.jsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { useRouter } from "next/navigation"
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useMemo, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import getMatch from "../../../get/client/getMatch";
+import LoadingSpinner from "../../../components/LoadingSpinner";
 
 export default function ClientMatchPage({ matchID }) {
-  const { data: match, isError, isInitialLoading } = useQuery({
+  const queryClient = useQueryClient();
+  const { data: match, isError, isInitialLoading, isLoading } = useQuery({
     queryKey: [`${matchID}`],
     queryFn: async () => {
       const { data } = await getMatch(matchID);
@@ -20,13 +22,29 @@ export default function ClientMatchPage({ matchID }) {
   }
 
   const router = useRouter();
-  const [set, setVariant] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
+  const [set, setSet] = useState(1);
+
+  function getCurrentSetFromRemote(match) {
+    let curr = 0;
+    console.log(match)
+    while (curr < match.sets.length) {
+      if (!match.sets[curr].finished) {
+        return curr + 1;
+      }
+      curr++;
+    }
+
+    return curr;
+  }
+
+  const currentSet = useMemo(() => isLoading ? set : (Math.max(set, getCurrentSetFromRemote(match))), [match, set])
 
   const onSubmit = (data) => {
-
+    queryClient.invalidateQueries({ queryKey: [`${matchID}`]})
   }
-  return (
-    <></>
+  return isLoading ? <LoadingSpinner /> : (
+    <div className="bg-slate-500">
+      <h1>{`Set ${set}`}</h1>
+    </div>
   )
 }
